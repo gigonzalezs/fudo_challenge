f206e31361312ad7b9369885e4e8b2290c7982fa
Gilbert Gonzalez <gilbert.uy.gonzalez@gmail.com>
2023-08-11 11:01:53 -0300

    spread implementation

--- lib/pages/home5/widgets/posts.dart
+++ lib/pages/home5/widgets/posts.dart
@@ -1,7 +1,9 @@
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:fudo_challenge/framework/spread_builder.dart';
 
+import '../../../framework/spread_state.dart';
 import '../home5_states.dart';
 
 class Posts5Page extends StatelessWidget {
@@ -9,13 +11,23 @@ class Posts5Page extends StatelessWidget {
   const Posts5Page({super.key, required this.posts});
 
   @override
-  Widget build(BuildContext context) => Spread<Posts5State>(
-      entity: posts,
-      builder: (BuildContext context, Posts5State? state) {
-        return Center(
-          child: Text(state?.count.toString() ?? "<POSTS>"),
-        );
-      }
+  Widget build(BuildContext context) => Center(
+    child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Spread<Posts5State>(
+              entity: posts,
+              builder: (BuildContext context, Posts5State? state) {
+                return Text(state?.count.toString() ?? "<POSTS>");
+              }),
+          IconButton(
+              onPressed: () {
+                print('increment posts');
+                posts.increment();
+                SpreadState().emitEntity(posts);
+              },
+              icon: const Icon(Icons.add_circle))
+        ],
+      )
   );
-
 }
--- lib/pages/home5/widgets/users.dart
+++ lib/pages/home5/widgets/users.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fudo_challenge/framework/spread_builder.dart';
@@ -9,27 +8,25 @@ import '../home5_states.dart';
 class Users5Page extends StatelessWidget {
   final Users5State users;
   const Users5Page({super.key, required this.users});
-  
-  @override
-  Widget build(BuildContext context) => Spread<Users5State>(
-      entity: users,
-      builder: (BuildContext context, Users5State? state) {
-        return Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(state?.count.toString() ?? "<USERS>"),
-              IconButton(
-                  onPressed: () {
-                    print('increment users');
-                    users.increment();
-                    SpreadState().emitEntity(users);
-                  },
-                  icon: const Icon(Icons.add_circle))
-            ],
-          )
-        );
-      }
-  );
 
+  @override
+  Widget build(BuildContext context) => Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Spread<Users5State>(
+              entity: users,
+              builder: (BuildContext context, Users5State? state) {
+                return Text(state?.count.toString() ?? "<USERS>");
+              }),
+          IconButton(
+              onPressed: () {
+                print('increment users');
+                users.increment();
+                SpreadState().emitEntity(users);
+              },
+              icon: const Icon(Icons.add_circle))
+        ],
+      )
+  );
 }
