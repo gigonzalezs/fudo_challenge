b3668409eb53d20c929624af909cca94af6d7223
Gilbert Gonzalez <gilbert.uy.gonzalez@gmail.com>
2023-08-15 19:25:38 -0300

    user_item effects

--- lib/users/widgets/user_item.dart
+++ lib/users/widgets/user_item.dart
@@ -5,71 +5,70 @@ import '../model/user.dart';
 class UserItem extends StatelessWidget with StateEmitter {
   final User user;
   bool _selected = false;
+  Color backColor = _tileColor;
 
   UserItem({super.key, required this.user});
 
   static const Color _tileColor = Colors.transparent;
-  static final Color _hoverColor = Colors.blue[100]!;
   static final Color _tapColor = Colors.blue[300]!;
 
   @override
   Widget build(BuildContext context) {
     return Spread<_UserItemState>(
         builder: (BuildContext ctx, _UserItemState? state) {
-      late Color backColor;
+
       if (state == null) {
         backColor = _tileColor;
       } else {
-        switch (state) {
-          case _UserItemState.NoSelected:
+
+        switch (state.runtimeType) {
+          case _UserNoSelectedState:
             {
-              backColor = _tileColor;
+              if (state.user == user) {
+                backColor = _tileColor;
+              }
               break;
             }
-          case _UserItemState.Selected:
+          case _UserSelectedState:
             {
-              backColor = _tapColor;
-              break;
-            }
-          case _UserItemState.HoverIn:
-            {
-              backColor = _hoverColor;
-              break;
-            }
-          case _UserItemState.HoverOut:
-            {
-              backColor = _tileColor;
+              if (state.user == user) {
+                backColor = _tapColor;
+              } else {
+                backColor = _tileColor;
+              }
               break;
             }
         }
       }
-      return MouseRegion(
-          onEnter: (_) => emit<_UserItemState>(_UserItemState.HoverIn),
-          onExit: (_) => emit<_UserItemState>(_UserItemState.HoverOut),
-          child: InkWell(
-            onTap: () => _toggleSelected(),
-              child: ListTile(
-              tileColor: _tileColor, //backColor,
+      return InkWell(
+          onTap: () => _toggleSelected(),
+          child: ListTile(
+              tileColor: backColor,
               title: Text(user.name),
               subtitle: Row(
                 children: [Text('${user.userName} <${user.email}>')],
-              )
-          )
-          )
-      );
+              )));
     });
   }
 
   void _toggleSelected() {
     _selected = !_selected;
     if (_selected) {
-      emit<_UserItemState>(_UserItemState.Selected);
+      emit<_UserItemState>(_UserSelectedState(user: user));
     } else {
-      emit<_UserItemState>(_UserItemState.NoSelected);
+      emit<_UserItemState>(_UserNoSelectedState(user: user));
     }
   }
 }
 
-enum _UserItemState {
-  NoSelected, Selected, HoverIn, HoverOut
+abstract class _UserItemState {
+  final User user;
+
+  _UserItemState({required this.user});
+}
+class _UserNoSelectedState extends _UserItemState {
+  _UserNoSelectedState({required super.user});
+}
+class _UserSelectedState extends _UserItemState {
+  _UserSelectedState({required super.user});
 }
