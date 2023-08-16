30d2f58f645a64e957f6e62af1077aa758c63cdd
Gilbert Gonzalez <gilbert.uy.gonzalez@gmail.com>
2023-08-15 21:25:38 -0300

    create post

--- lib/posts/model/post.dart
+++ lib/posts/model/post.dart
@@ -3,14 +3,16 @@ import '../../ports/ports.dart';
 
 class Post {
   final int id;
+  final int userId;
   final String title;
   final String body;
 
-  Post({required this.id, required this.title, required this.body});
+  Post({required this.id, required this.userId, required this.title, required this.body});
 
   factory Post.fromDTO(PostDTO dto) {
     return Post(
       id: dto.id,
+      userId: dto.userId,
       title: dto.title,
       body: dto.body,
     );
--- lib/posts/presentation/create_post_page.dart
+++ lib/posts/presentation/create_post_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:spread/spread.dart';
 
 import '../../pages/home/states.dart';
+import '../../users/states/user_item_states.dart';
+import '../states/post_states.dart';
 
 class CreatePostPage extends StatelessWidget with StateEmitter {
   final _formKey = GlobalKey<FormState>();
@@ -10,72 +12,81 @@ class CreatePostPage extends StatelessWidget with StateEmitter {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Crear Post'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                controller: _titleController,
-                decoration: InputDecoration(labelText: 'Título'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor ingresa un título';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 16),
-              TextFormField(
-                controller: _bodyController,
-                decoration: InputDecoration(labelText: 'Cuerpo'),
-                maxLines: null,
-                keyboardType: TextInputType.multiline,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor ingresa el cuerpo del post';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        // Aquí puedes manejar la lógica para guardar el post
-                        print('Título: ${_titleController.text}');
-                        print('Cuerpo: ${_bodyController.text}');
-                        emit<AppState>(AppState.posts);
+    return Spread<UserItemState>(builder: (BuildContext context, UserItemState? state) {
+      if (state is UserSelectedState) {
+        final user = state.user;
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Crear Post para ${user.name}'),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    controller: _titleController,
+                    decoration: InputDecoration(labelText: 'Título'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Por favor ingresa un título';
                       }
+                      return null;
                     },
-                    child: Text('Guardar'),
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      // Aquí puedes manejar la lógica para cancelar
-                      _titleController.clear();
-                      _bodyController.clear();
-                      emit<AppState>(AppState.posts);
+                  SizedBox(height: 16),
+                  TextFormField(
+                    controller: _bodyController,
+                    decoration: InputDecoration(labelText: 'Cuerpo'),
+                    maxLines: null,
+                    keyboardType: TextInputType.multiline,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Por favor ingresa el cuerpo del post';
+                      }
+                      return null;
                     },
-                    child: Text('Cancelar'),
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.red,
-                    ),
+                  ),
+                  SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            // Aquí puedes manejar la lógica para guardar el post
+                            print('Título: ${_titleController.text}');
+                            print('Cuerpo: ${_bodyController.text}');
+                            emit<AppState>(AppState.posts);
+                          }
+                        },
+                        child: Text('Guardar'),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          // Aquí puedes manejar la lógica para cancelar
+                          _titleController.clear();
+                          _bodyController.clear();
+                          emit<AppState>(AppState.posts);
+                        },
+                        child: Text('Cancelar'),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.red,
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
+            ),
           ),
-        ),
-      ),
-    );
+        );
+      } else {
+        return const Center(
+          child: Text('No hay usuario seleccionado'),
+        );
+      }
+    });
   }
 }
